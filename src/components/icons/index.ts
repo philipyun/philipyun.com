4553ebef1b4723dd01df716299c4bc21939d39bb
Philip Yun <philip.yun103@gmail.com>
2024-01-23 08:12:03 -0800

    add icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,9 @@
+'use client';
+
+export * from './github';
+export * from './instagram';
+export * from './linkedin';
+export * from './spotify';
+export * from './twitch';
+export * from './x';
+export * from './youtube';
